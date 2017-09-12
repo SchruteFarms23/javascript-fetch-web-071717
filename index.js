e6075551ede76aebe090670bea9cac75570dfdbd
SchruteFarms23 <romanmullady@gmail.com>
2017-09-12 08:07:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+console.log(app)
+
+let bird = fetch('https://api.github.com/repos/jquery/jquery/commits')
+  .then(res => res.json())
+  .then(json => json);
+
+  console.log(bird)
